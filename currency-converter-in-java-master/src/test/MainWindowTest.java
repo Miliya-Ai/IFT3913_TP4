6309db565c45f6560ec0d456e958d37b22de413c
kim841 <kim.trinh@umontreal.ca>
2023-12-02 20:22:48 -0500

    Main window test

--- currency-converter-in-java-master/src/test/MainWindowTest.java
+++ currency-converter-in-java-master/src/test/MainWindowTest.java
@@ -1,12 +1,41 @@
 package test;
 
+import currencyConverter.Currency;
+import currencyConverter.MainWindow;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainWindowTest {
-
+    public ArrayList<Currency> currencies;
+    @BeforeEach
+    void setUp() {
+        currencies = Currency.init();
+    }
     @Test
-    void convert() {
+    void convertPartition() {
+       /*
+       Devise acceptée
+        */
+        assertDoesNotThrow(() -> MainWindow.convert("Euro", "US Dollar", currencies, 1.0));
+        assertDoesNotThrow(() -> MainWindow.convert("Canadian dollar", "US Dollar", currencies, 1.0));
+        assertDoesNotThrow(() -> MainWindow.convert("Sterling", "US Dollar", currencies, 1.0));
+        assertDoesNotThrow(() -> MainWindow.convert("Swiss franc", "US Dollar", currencies, 1.0));
+        assertDoesNotThrow(() -> MainWindow.convert("Australian dollar", "US Dollar", currencies, 1.0));
+
+//        System.out.println(MainWindow.convert("Euro", "US Dollar", currencies, 1.0));
+//        System.out.println(MainWindow.convert("Canadian dollar", "US Dollar", currencies, 1.0));
+//        System.out.println(MainWindow.convert("Sterling", "US Dollar", currencies, 1.0));
+//        System.out.println(MainWindow.convert("Swiss franc", "US Dollar", currencies, 1.0));
+//        System.out.println(MainWindow.convert("Australian dollar", "US Dollar", currencies, 1.0));
+
+        /*
+        Devise non acceptée
+         */
+        assertThrows(IllegalArgumentException.class, () -> MainWindow.convert("Japanese yen", "US Dollar", currencies, 1.0));
+//        System.out.println(MainWindow.convert("Japanese yen", "US Dollar", currencies, 1.0));
     }
 }

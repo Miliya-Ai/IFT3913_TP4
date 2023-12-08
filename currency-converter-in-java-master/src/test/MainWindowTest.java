bf215b0e65eb70e3c386f49b7db8f2adcbe5ac4f
Miliya-Ai <102935805+Miliya-Ai@users.noreply.github.com>
2023-12-07 21:09:58 -0500

    Critère de couverture des conditions et Critère de couverture des instructions

--- currency-converter-in-java-master/src/test/MainWindowTest.java
+++ currency-converter-in-java-master/src/test/MainWindowTest.java
@@ -38,4 +38,13 @@ class MainWindowTest {
         assertThrows(IllegalArgumentException.class, () -> MainWindow.convert("Japanese yen", "US Dollar", currencies, 1.0));
 //        System.out.println(MainWindow.convert("Japanese yen", "US Dollar", currencies, 1.0));
     }
+
+    @Test
+    public void testInstructionCoverage(){
+        assertEquals(1.0, MainWindow.convert("US Dollar", "US Dollar", currencies, 1.0));
+        assertEquals(1.07, MainWindow.convert("Euro", "US Dollar", currencies, 1.0));
+        assertEquals(1.51, MainWindow.convert("British Pound", "US Dollar", currencies, 1.0));
+        assertEquals(0.0, MainWindow.convert("Dollar exist pas", "US Dollar", currencies, 1.0));
+        assertEquals(0.0, MainWindow.convert("US Dollar", "Dollar exist pas", currencies, 1.0));
+    }
 }
